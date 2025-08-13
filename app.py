a831ce627ceb9170dcbaec5e49b8a72cf94d69e3
Minnu Antony <minnu.antony05@gmail.com>
2025-08-13 17:56:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ html = """
 <!doctype html>
 <html>
 <head>
-    <title>Expense Tracker Hackathon</title>
+    <title>Expense Tracker Hackathon Hi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="p-4">
